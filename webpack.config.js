da67e69fb41003e29d5f656a30ad6d6b7b68fd82
niqdev <niqdev@gmail.com>
2016-07-05 23:35:41 +0200

    add html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,24 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: [
+    'webpack-dev-server/client?http://localhost:8080',
     './src/index.js'
   ],
   output: {
     path: path.join(__dirname, 'public'),
-    publicPath: '/',
     filename: 'app.js'
   },
   devServer: {
     contentBase: './public'
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'react-bootstrap4-ghpages',
+      filename: 'index.html',
+      template: 'index.template.html',
+      favicon: path.join(__dirname, 'public/favicon.ico')
+    })
+  ]
 };
