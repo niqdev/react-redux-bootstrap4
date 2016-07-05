afd8131c3aabcd9489a97a6af69e4dbe673efab7
niqdev <niqdev@gmail.com>
2016-07-05 23:27:07 +0200

    add webpack dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: [
+    './src/index.js'
+  ],
   output: {
     path: path.join(__dirname, 'public'),
+    publicPath: '/',
     filename: 'app.js'
+  },
+  devServer: {
+    contentBase: './public'
   }
 };
