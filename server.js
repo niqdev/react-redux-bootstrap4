04624fe30d450358b0bea6d6cc50b2f0afa7bbce
niqdev <niqdev@gmail.com>
2016-07-06 09:36:24 +0200

    open browser on start

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
 const config = require('./webpack.config');
+const open = require('open');
 
 const server = new WebpackDevServer(webpack(config), {
   contentBase: config.output.path,
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true,
-  stats: {colors: true}
+  stats: {
+    colors: true
+  }
 });
 
 server.listen(3000, 'localhost', function (err, result) {
@@ -15,4 +18,5 @@ server.listen(3000, 'localhost', function (err, result) {
     return console.log(err);
   }
   console.log('Listening at http://localhost:3000/');
+  open('http://localhost:3000');
 });
