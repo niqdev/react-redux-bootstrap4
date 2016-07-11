01fa101f84e8645252d8972d2558dffb319c2f49
niqdev <niqdev@gmail.com>
2016-07-11 19:09:57 +0200

    v0.4.0

--- src/containers/Root.js
+++
@@ -1,7 +0,0 @@
-/*eslint-disable */
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./Root.prod')
-} else {
-  module.exports = require('./Root.dev')
-}
-/*eslint-enable */
