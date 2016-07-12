fa341e87bfdaea92bad9decc1d0c332cc2d8a71f
niqdev <niqdev@gmail.com>
2016-07-12 09:17:53 +0200

    add counter route

--- src/core/App.js
+++ src/core/App.js
@@ -1,14 +1,13 @@
 import React, { PropTypes } from 'react'
 import { Link } from 'react-router'
-import { Counter } from '../modules'
 
 export default function App({ children }) {
   return (
     <div className="container">
       <div className="jumbotron">
         <h1>react-redux-bootstrap4</h1>
-        <Counter />
         <Link to="/gallery">Gallery</Link>
+        <Link to="/counter">Counter</Link>
         {children}
       </div>
     </div>
--- src/core/routes.js
+++ src/core/routes.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 import App from './App'
+import { Counter } from '../modules'
 
 const Index = () => <div>INDEX</div>
 const Gallery = () => <div>GALLERY</div>
@@ -10,6 +11,7 @@ export default (
   <Route path="/" component={App}>
     <IndexRoute component={Index} />
     <Route path="gallery" component={Gallery} />
+    <Route path="counter" component={Counter} />
     <Route path="*" component={NotFound} />
   </Route>
 )
