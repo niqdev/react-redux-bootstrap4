eb6590ce59ccdffdf67093c062eb0cd7c91dddeb
niqdev <niqdev@gmail.com>
2016-07-18 19:20:20 +0200

    update locales

--- src/modules/counter/CounterComponent.js
+++ src/modules/counter/CounterComponent.js
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react'
+import { FormattedMessage } from 'react-intl'
 import './counter.scss'
 
 export default class Counter extends Component {
@@ -11,14 +12,17 @@ export default class Counter extends Component {
     this.props.actions.decrement()
   }
 
+  getMessage() {
+    return this.props.counter % 2 === 0 ? <FormattedMessage id="counter.even" /> :
+      <FormattedMessage id="counter.odd" />
+  }
+
   render() {
     return (
       <div className="my-counter center-block">
         <div className="row m-b-2">
           <div className="col-xs-6">{this.props.counter}</div>
-          <div className="col-xs-6 counter-label">{
-            this.props.counter % 2 === 0 ? 'even' : 'odd'}
-          </div>
+          <div className="col-xs-6 counter-label">{this.getMessage()}</div>
         </div>
         <div className="row center-block">
           <div className="col-xs-6">
--- src/modules/home/HomeComponent.js
+++ src/modules/home/HomeComponent.js
@@ -1,13 +1,20 @@
 import React from 'react'
+import { FormattedMessage } from 'react-intl'
 
 export default function HomeComponent() {
   return (
-    <div>
-      <div className="jumbotron">
-        <h1 className="display-3">Boilerplate</h1>
-        <p className="lead">react-redux-bootstrap4</p>
-      </div>
-      <pre>https://github.com/niqdev/react-redux-bootstrap4</pre>
+    <div className="jumbotron">
+      <h1 className="display-3">Boilerplate</h1>
+      <p className="lead">react-redux-bootstrap4</p>
+      <hr className="m-y-2"/>
+      <p className="lead">
+        <a
+          className="btn btn-primary btn-lg"
+          href="https://github.com/niqdev/react-redux-bootstrap4"
+          role="button">
+          <FormattedMessage id="home.source" />
+        </a>
+      </p>
     </div>
   )
 }
